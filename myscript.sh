67bd984d1335cad099c528a07f34ee18279758e7
Sonal Mitra <mitrasonal130996@gmail.com>
2023-08-07 20:58:46 +0530

    make it function and apply infinite loop with sleep 2

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-
+usedpercent() {
 totalsize=$(df | grep -w "/" | awk '{print $3}')
 used=$(df | grep -w "/" | awk '{print $4}')
 avail=$(df | grep -w "/" | awk '{print $5}')
@@ -9,6 +9,12 @@ echo "used data in percentage= $(($used*100/$totalsize))%"
 
 
 echo "available data in percentage= $(($avail*100/$totalsize))%"
+}
+while true
+do
+	usedpercent
+	sleep 2
+done
 
 
 
